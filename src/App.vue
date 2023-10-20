73d435f952df7f7c56dcfa3f2501ce3451114e8c
Integral <integral@murena.io>
2023-10-21 00:43:54 +0800

    Tweak login page & format code

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts"></script>
 
 <template>
-  <router-link :to="{ name: 'HelloWorld' }">Login</router-link>
-  <router-link style="margin-left: 10px" :to="{ name: 'UI' }">UI</router-link>
+  <div id="nav">
+    <router-link :to="{ name: 'HelloWorld' }">Login</router-link>
+    <router-link style="margin-left: 10px" :to="{ name: 'UI' }">UI</router-link>
+  </div>
   <router-view></router-view>
 </template>
 
@@ -18,5 +20,24 @@
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  padding-right: 10px;
+}
+
+#nav a:not(:first-child) {
+  border-left: 1px solid #2c3e50;
+  padding-left: 30px;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,21 @@
 <template>
   <router-view></router-view>
   <div>
+    <img src="/wordhub.png" width="100" height="100" />
     <h2>用户登录</h2>
-    <form @submit.prevent="login">
+    <form>
       <div>
-        <label for="username">用户名:</label>
-        <input type="text" id="username" v-model="username" required />
+        <label for="username">用户名：</label>
+        <input type="text" id="username" v-model="username" />
       </div>
+      <br />
       <div>
-        <label for="password">密码:</label>
-        <input type="password" id="password" v-model="password" required />
+        <label for="password">密码：</label>
+        <input type="password" id="password" v-model="password" />
       </div>
-      <button type="submit">登录</button>
+      <br />
     </form>
+    <button @click="login">登录</button>
   </div>
 
   <!-- <div> <router-link :to="{name:'UI'}" >Login</router-link></div> -->
@@ -25,12 +28,38 @@ const username = ref("");
 const password = ref("");
 
 function login() {
-  // 在这里处理登录逻辑，例如验证用户信息
-  if (username.value === "user" && password.value === "password") {
+  if (username.value === "" || password.value === "")
+    alert("请输入用户名和密码");
+  else if (username.value === "user" && password.value === "password")
     alert("登录成功");
-  } else {
-    alert("登录失败");
-  }
+  else alert("用户名或密码错误");
 }
 </script>
-<style scoped></style>
+
+<style scoped>
+form {
+  display: table;
+}
+
+div {
+  display: table-row;
+}
+
+label {
+  display: table-cell;
+  text-align: right;
+}
+
+input {
+  display: table-cell;
+}
+
+.dropbtn {
+  background-color: #4caf50;
+  color: white;
+  padding: 16px;
+  font-size: 16px;
+  border: none;
+  cursor: pointer;
+}
+</style>
