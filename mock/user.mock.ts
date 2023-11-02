5db08336ff28ef9b97fb8308fc04050a7ac536f2
Integral <integral@murena.io>
2023-11-02 17:42:48 +0800

    Add mock/ to format directory list & format mock/

--- mock/user.mock.ts
+++ mock/user.mock.ts
@@ -1,20 +1,20 @@
-import { defineMock } from 'vite-plugin-mock-dev-server'
-import {UserVo} from '../src/store/loginStore';
-const user:UserVo ={
-    id: 1,
-    username: 'testUser',
-    email: '123@456.com',
-    score: 0,
-    role: 0
-} 
+import { defineMock } from "vite-plugin-mock-dev-server";
+import { UserVo } from "../src/store/loginStore";
+const user: UserVo = {
+  id: 1,
+  username: "testUser",
+  email: "123@456.com",
+  score: 0,
+  role: 0,
+};
 
 export default defineMock([
   {
-    url: '/api/session',
-    method: 'POST',
+    url: "/api/session",
+    method: "POST",
     status: 200,
     enabled: true,
-    type: 'json',
-    body: user
-  }
-])
+    type: "json",
+    body: user,
+  },
+]);
