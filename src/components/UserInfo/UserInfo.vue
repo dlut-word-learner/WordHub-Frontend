4e3e11e65f946985ae85eeac6b28dd92bf8741c2
Integral <integral@murena.io>
2023-11-06 09:32:30 +0800

    UserInfo.vue: el-row/el-rol -> el-container/el-aside

--- src/components/UserInfo/UserInfo.vue
+++ src/components/UserInfo/UserInfo.vue
@@ -1,6 +1,6 @@
 <template>
-  <el-row>
-    <el-col :span="navSpan">
+  <el-container>
+    <el-aside :width="sideWidth">
       <el-menu id="menu" default-active="/user-info/basic" :router="true">
         <el-menu-item index="/user-info/basic">
           <div class="navItem">{{ $t("userInfo.nav.basic") }}</div>
@@ -15,33 +15,28 @@
           <div class="navItem">{{ $t("userInfo.nav.deleteUser") }}</div>
         </el-menu-item>
       </el-menu>
-    </el-col>
-    <el-col :span="24 - navSpan">
+    </el-aside>
+    <el-main>
       <router-view></router-view>
-    </el-col>
-  </el-row>
+    </el-main>
+  </el-container>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
 import { i18n } from "../../main";
 import router from "../../router";
+import { computed } from "vue";
 
-const navSpan = ref(0);
-
-switch (i18n.global.locale.value) {
-  case "zh_cn":
-    navSpan.value = 2;
-    break;
-
-  case "en":
-    navSpan.value = 3;
-    break;
-
-  case "ja":
-    navSpan.value = 4;
-    break;
-}
+const sideWidth = computed(() => {
+  switch (i18n.global.locale.value) {
+    case "zh_cn":
+      return "120px";
+    case "en":
+      return "180px";
+    case "ja":
+      return "200px";
+  }
+});
 
 router.push("/user-info/basic");
 </script>
