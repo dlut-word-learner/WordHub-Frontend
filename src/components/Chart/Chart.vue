2f87c57f6e549eb757882a46c39d017237ca2ce1
eyesofish <110754016+eyesofish@users.noreply.github.com>
2023-11-08 10:57:10 +0800

    piechart

--- src/components/Chart/Chart.vue
+++ src/components/Chart/Chart.vue
@@ -15,14 +15,36 @@ const initChart = () => {
     myChart = echarts.init(chartContainer.value);
     const option: echarts.EChartsOption = {
       // 在这里放置你的图表配置
+      title: {
+    text: 'Referer of a Website',
+    subtext: 'Fake Data',
+    left: 'center'
+  },
+  tooltip: {
+    trigger: 'item'
+  },
       xAxis: {},
       yAxis: {},
       series: [
-        {
-          type: "line",
-          smooth: true,
-          data: [30, 40, 20, 50, 40, 80, 90],
-        },
+      {
+      name: 'Access From',
+      type: 'pie',
+      radius: '50%',
+      data: [
+        { value: 1048, name: 'Search Engine' },
+        { value: 735, name: 'Direct' },
+        { value: 580, name: 'Email' },
+        { value: 484, name: 'Union Ads' },
+        { value: 300, name: 'Video Ads' }
+      ],
+      emphasis: {
+        itemStyle: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: 'rgba(0, 0, 0, 0.5)'
+        }
+      }
+    }
       ],
     };
     myChart.setOption(option);
