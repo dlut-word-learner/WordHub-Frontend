949edbaf247d1dec8b0a71ff888d6540ceee5073
Integral <integral@murena.io>
2023-11-09 23:58:58 +0800

    Fix katakana issue in Japanese

--- src/components/WordCard.ts
+++ src/components/WordCard.ts
@@ -46,24 +46,26 @@ export function getHiddenWord(word: WordVo, input: string, lang: Lang): string {
       return input + "_ ".repeat(word.name.length - input.length);
 
     case Lang.Japanese:
-      return isKatakana(word.extension.notation)
-        ? toKatakana(input)
-        : toHiragana(input) +
-            "__".repeat(
-              Math.max(
-                0,
-                toKana(word.name).length -
-                  toKana(input)
-                    .split("")
-                    .filter((x) => isKana(x))
-                    .join("").length -
-                  toKana(input)
-                    .split("")
-                    .filter((x) => !isKana(x))
-                    .join("").length /
-                    3,
-              ),
-            );
+      return (
+        (isKatakana(word.extension.notation)
+          ? toKatakana(input)
+          : toHiragana(input)) +
+        "__".repeat(
+          Math.max(
+            0,
+            toKana(word.name).length -
+              toKana(input)
+                .split("")
+                .filter((x) => isKana(x))
+                .join("").length -
+              toKana(input)
+                .split("")
+                .filter((x) => !isKana(x))
+                .join("").length /
+                3,
+          ),
+        )
+      );
     default:
       return "";
   }
