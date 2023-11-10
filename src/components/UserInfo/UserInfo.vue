fbe388bcfddba2deafe3f5e0d61d69ddabd25efa
Integral <integral@murena.io>
2023-11-10 20:49:52 +0800

    UserInfo.vue: change layout

--- src/components/UserInfo/UserInfo.vue
+++ src/components/UserInfo/UserInfo.vue
@@ -2,17 +2,17 @@
   <el-container>
     <el-aside :width="sideWidth">
       <el-menu id="menu" default-active="/user-info/basic" :router="true">
-        <el-menu-item index="/user-info/basic">
-          <div class="menu-item">{{ $t("userInfo.nav.basic") }}</div>
+        <el-menu-item index="/user-info/basic" class="menu-item">
+          <div>{{ $t("userInfo.nav.basic") }}</div>
         </el-menu-item>
-        <el-menu-item index="/user-info/change-pwd">
-          <div class="menu-item">{{ $t("userInfo.nav.changePwd") }}</div>
+        <el-menu-item index="/user-info/change-pwd" class="menu-item">
+          <div>{{ $t("userInfo.nav.changePwd") }}</div>
         </el-menu-item>
-        <el-menu-item index="/user-info/change-avatar">
-          <div class="menu-item">{{ $t("userInfo.nav.changeAvatar") }}</div>
+        <el-menu-item index="/user-info/change-avatar" class="menu-item">
+          <div>{{ $t("userInfo.nav.changeAvatar") }}</div>
         </el-menu-item>
-        <el-menu-item index="/user-info/delete-user">
-          <div class="menu-item">{{ $t("userInfo.nav.deleteUser") }}</div>
+        <el-menu-item index="/user-info/delete-user" class="menu-item">
+          <div>{{ $t("userInfo.nav.deleteUser") }}</div>
         </el-menu-item>
       </el-menu>
     </el-aside>
@@ -30,14 +30,13 @@ import { computed } from "vue";
 const sideWidth = computed(() => {
   switch (i18n.global.locale.value) {
     case "zh_cn":
-      return "120px";
+      return "max(12vh, 6vw)";
     case "en":
-      return "180px";
+      return "max(20vh, 10vw)";
     case "ja":
-      return "200px";
+      return "max(24vh, 12vw)";
   }
 });
-
 router.push("/user-info/basic");
 </script>
 
@@ -47,7 +46,10 @@ router.push("/user-info/basic");
 }
 
 .menu-item {
-  margin: auto auto;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0);
+  height: max(8vh, 4vw);
   font-size: max(2vh, 1vw);
 }
 
