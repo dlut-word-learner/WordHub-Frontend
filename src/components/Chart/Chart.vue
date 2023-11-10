df7a5431aba90c4f91d619c8694456ca4ce70264
OuOu2021 <1113117424@qq.com>
2023-11-10 13:25:27 +0800

    Fix: Charts' binding

--- src/components/Chart/Chart.vue
+++ src/components/Chart/Chart.vue
@@ -3,21 +3,26 @@
     <el-main id="barCharts">
       <el-row>
         <el-col v-for="task in tasks" :span="8"
-          ><div class="barChart" :ref="(ele) => (barChartsRef[task] = ele)"></div
+          ><div
+            class="barChart"
+            :ref="(ele) => (barChartsRef[task] = ele as HTMLElement)"
+          ></div
         ></el-col>
       </el-row>
     </el-main>
     <el-main id="secondRow">
       <el-row>
         <el-col
-          ><div id="heatMap" :ref="ele => {heatMapRef = ele  as HTMLElement}"></div
+          ><div
+            id="heatMap"
+            :ref="(ele) => (heatMapRef = ele as HTMLElement)"
+          ></div
         ></el-col>
         <el-col>
-          <div v-for="(_dict, index) in dictsToGenerateProgress" class="progress">
-            <div
-              :ref="(ele) => {progressRef[index] = ele as HTMLElement}"
-            ></div>
-          </div>
+          <div
+            :ref="(ele) => (progressRef = ele as HTMLElement)"
+            class="progress"
+          ></div>
         </el-col>
       </el-row>
     </el-main>
@@ -25,7 +30,7 @@
 </template>
 
 <script setup lang="ts">
-import { Ref, onMounted, reactive, ref } from "vue";
+import { onMounted, reactive, ref } from "vue";
 import * as echarts from "echarts";
 import axios from "axios";
 import { useHistoryStore } from "../../store/historyStore";
@@ -42,38 +47,43 @@ const dictsToGenerateProgress: DictVo[] = historyStore.recentlyUsedDicts.slice(
 const tasks = [Task.Learn, Task.Review, Task.QwertyMode];
 
 // 图表div元素绑定
-const heatMapRef: Ref<HTMLElement | null> = ref(null);
+const heatMapRef = ref<HTMLElement>();
 const barChartsRef = reactive(new Map<Task, HTMLElement>());
-const progressRef = reactive<HTMLElement[]>([]);
+const progressRef = ref<HTMLElement>();
 
 // 实际ECharts图表
 let heatMap: echarts.ECharts;
-let barCharts= new Map<Task, echarts.ECharts>();
-let progress: echarts.ECharts[] = [];
+let barCharts = new Map<Task, echarts.ECharts>();
+let progress: echarts.ECharts;
 
 // 声明要从后端API取得的数据
 
 const initChart = () => {
+  console.log("heatMapRef: ");
+  console.log(heatMapRef.value);
+  console.log("progressRef: ");
+  console.log(progressRef.value);
+  for (const task of tasks) {
+    initbarchart(task);
+  }
   if (progressRef) {
-    for (let i = 0; i < 10; i++) {
-      if (progressRef[i] != undefined) {
-        console.debug("recentlyUsed: " + i);
-        progress.push(echarts.init(progressRef[i]));
-        initProgress(i);
-      } else break;
-    }
+    progress = echarts.init(progressRef.value);
+    // for (let i = 0; i < 10; i++) {
+    //   if (progressRef[i] != undefined) {
+    //     console.debug("recentlyUsed: " + i);
+
+    //     initProgress(i);
+    //   } else break;
+    // }
   }
   initheatchart();
-  initbarchart(Task.Learn);
-  initbarchart(Task.Review);
-  initbarchart(Task.QwertyMode);
 };
 //for bar chart ,generate fake datas.
 function getVirtualData(): Array<[string, string]> {
   const data: Array<[string, string]> = []; // 声明一个数组，其中的元素是键值对数组
 
   for (let i = 1; i <= 30; i++) {
-    const date = `11/${i < 10 ? '0' + i : i}`; // 格式化日期字符串，补全个位数日期
+    const date = `11/${i < 10 ? "0" + i : i}`; // 格式化日期字符串，补全个位数日期
     const randomValue = Math.floor(Math.random() * 10000).toString(); // 生成随机数并转换为字符串
     data.push([date, randomValue]); // 将键值对推入data数组中
   }
@@ -82,32 +92,31 @@ function getVirtualData(): Array<[string, string]> {
 }
 
 function initbarchart(task: Task): void {
-  console.log(task + ": " + barChartsRef.has(task));
-  console.log(heatMapRef[task]);
-  if (barChartsRef[task] != undefined) {
-    console.log("yes");
+  if (barChartsRef[task]) {
+    console.log(task);
+    console.log(barChartsRef[task]);
     barCharts[task] = echarts.init(barChartsRef[task]);
     const option: echarts.EChartsOption = {
       xAxis: {
-    type: "category",
-    data: getVirtualData().map(item => item[0]), // 获取虚拟数据中的第一个元素作为x轴数据
-  },
-  yAxis: {
-    type: "value",
-  },
-  series: [
-    {
-      data: getVirtualData().map(item => ({
-        name: item[0], // 使用日期作为名称
-        value: parseInt(item[1]), // 将随机数字符串转换为整数
-      })),
-      type: "bar",
-      showBackground: true,
-      backgroundStyle: {
-        color: "rgba(180, 180, 180, 0.2)",
+        type: "category",
+        data: getVirtualData().map((item) => item[0]), // 获取虚拟数据中的第一个元素作为x轴数据
       },
-    },
-  ],
+      yAxis: {
+        type: "value",
+      },
+      series: [
+        {
+          data: getVirtualData().map((item) => ({
+            name: item[0], // 使用日期作为名称
+            value: parseInt(item[1]), // 将随机数字符串转换为整数
+          })),
+          type: "bar",
+          showBackground: true,
+          backgroundStyle: {
+            color: "rgba(180, 180, 180, 0.2)",
+          },
+        },
+      ],
     };
     barCharts[task].setOption(option);
   }
@@ -116,7 +125,7 @@ function initbarchart(task: Task): void {
 function generateData(): Array<[string, string]> {
   const data: Array<[string, string]> = [];
   for (let i = 1; i <= 90; i++) {
-    const date = `2023-${Math.floor(i / 30) + 9}-${i % 30 + 1}`;
+    const date = `2023-${Math.floor(i / 30) + 9}-${(i % 30) + 1}`;
     const randomValue = Math.floor(Math.random() * 10000).toString();
     data.push([date, randomValue]);
   }
@@ -124,41 +133,43 @@ function generateData(): Array<[string, string]> {
 }
 
 // heatmap生成模拟系列数据的函数
-function generateMockSeries(data: Array<[string, string]>): echarts.SeriesOption[] {
+function generateMockSeries(
+  data: Array<[string, string]>,
+): echarts.SeriesOption[] {
   const recentData = data.slice(-90);
 
   const series: echarts.SeriesOption[] = [
     {
-      name: 'Steps',
-      type: 'scatter',
-      coordinateSystem: 'calendar',
+      name: "Steps",
+      type: "scatter",
+      coordinateSystem: "calendar",
       data: recentData,
       symbolSize: function (val) {
         return val[1] / 500;
       },
       itemStyle: {
-        color: '#ddb926'
-      }
+        color: "#ddb926",
+      },
     },
     {
-      name: 'Top 12',
-      type: 'effectScatter',
-      coordinateSystem: 'calendar',
-      data:generateData(),
+      name: "Top 12",
+      type: "effectScatter",
+      coordinateSystem: "calendar",
+      data: generateData(),
       symbolSize: function (val) {
         return val[1] / 500;
       },
-      showEffectOn: 'render',
+      showEffectOn: "render",
       rippleEffect: {
-        brushType: 'stroke'
+        brushType: "stroke",
       },
       itemStyle: {
-        color: '#f4e925',
+        color: "#f4e925",
         shadowBlur: 10,
-        shadowColor: '#333'
+        shadowColor: "#333",
       },
-      zlevel: 1
-    }
+      zlevel: 1,
+    },
   ];
 
   return series;
@@ -167,7 +178,7 @@ function initheatchart(): void {
   if (heatMapRef.value) {
     console.log("yes");
     heatMap = echarts.init(heatMapRef.value);
-  
+
     const option: echarts.EChartsOption = {
       // 在这里放置你的图表配置
       backgroundColor: isDark ? "#404a59" : "#000",
@@ -238,10 +249,8 @@ function initheatchart(): void {
             borderColor: "#111",
           },
         },
-        
       ],
-      series: generateMockSeries(generateData())  ,// 使用类型断言
-      
+      series: generateMockSeries(generateData()), // 使用类型断言
     };
     heatMap.setOption(option);
   }
