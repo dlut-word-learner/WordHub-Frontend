72c90ef5f856577396b0796beadd1207003ad192
eyesofish <110754016+eyesofish@users.noreply.github.com>
2023-11-10 14:34:14 +0800

    follow the authority

--- src/components/Chart/Chart.vue
+++ src/components/Chart/Chart.vue
@@ -122,135 +122,168 @@ function initbarchart(task: Task): void {
   }
 }
 // heatmap生成模拟数据的函数
-function generateData(): Array<[string, string]> {
-  const data: Array<[string, string]> = [];
-  for (let i = 1; i <= 90; i++) {
-    const date = `2023-${Math.floor(i / 30) + 9}-${(i % 30) + 1}`;
-    const randomValue = Math.floor(Math.random() * 10000).toString();
-    data.push([date, randomValue]);
+function getheatmapVirtualData(year: string) {
+  const date = +echarts.time.parse(year + '-01-01');
+  const end = +echarts.time.parse(+year + 1 + '-01-01');
+  const dayTime = 3600 * 24 * 1000;
+  const data: [string, number][] = [];
+  for (let time = date; time < end; time += dayTime) {
+    data.push([
+      echarts.time.format(time, '{yyyy}-{MM}-{dd}', false),
+      Math.floor(Math.random() * 10000)
+    ]);
   }
   return data;
 }
 
-// heatmap生成模拟系列数据的函数
-function generateMockSeries(
-  data: Array<[string, string]>,
-): echarts.SeriesOption[] {
-  const recentData = data.slice(-90);
-
-  const series: echarts.SeriesOption[] = [
-    {
-      name: "Steps",
-      type: "scatter",
-      coordinateSystem: "calendar",
-      data: recentData,
-      symbolSize: function (val) {
-        return val[1] / 500;
-      },
-      itemStyle: {
-        color: "#ddb926",
-      },
-    },
-    {
-      name: "Top 12",
-      type: "effectScatter",
-      coordinateSystem: "calendar",
-      data: generateData(),
-      symbolSize: function (val) {
-        return val[1] / 500;
-      },
-      showEffectOn: "render",
-      rippleEffect: {
-        brushType: "stroke",
-      },
-      itemStyle: {
-        color: "#f4e925",
-        shadowBlur: 10,
-        shadowColor: "#333",
-      },
-      zlevel: 1,
-    },
-  ];
-
-  return series;
-}
+const data = getheatmapVirtualData('2016');
 function initheatchart(): void {
   if (heatMapRef.value) {
     console.log("yes");
     heatMap = echarts.init(heatMapRef.value);
 
     const option: echarts.EChartsOption = {
-      // 在这里放置你的图表配置
-      backgroundColor: isDark ? "#404a59" : "#000",
-      title: {
-        top: 30,
-        text: "Daily WORD Count in Last Three Months",
-
-        left: "center",
-        textStyle: {
-          color: isDark.value ? "#fff" : "#409EFF",
-        },
+      backgroundColor: '#404a59',
+  title: {
+    top: 30,
+    text: 'Daily Step Count in 2016',
+    subtext: 'Fake Data',
+    left: 'center',
+    textStyle: {
+      color: '#fff'
+    }
+  },
+  tooltip: {
+    trigger: 'item'
+  },
+  legend: {
+    top: '30',
+    left: '100',
+    data: ['Steps', 'Top 12'],
+    textStyle: {
+      color: '#fff'
+    }
+  },
+  calendar: [
+    {
+      top: 100,
+      left: 'center',
+      range: ['2016-01-01', '2016-06-30'],
+      splitLine: {
+        show: true,
+        lineStyle: {
+          color: '#000',
+          width: 4,
+          type: 'solid'
+        }
       },
-      tooltip: {
-        trigger: "item",
+      yearLabel: {
+        formatter: '{start}  1st',
+        color: '#fff'
       },
-      legend: {
-        top: "30",
-        left: "100",
-        data: ["Steps", "Top 12"],
-        textStyle: {
-          color: "#fff",
-        },
+      itemStyle: {
+        color: '#323c48',
+        borderWidth: 1,
+        borderColor: '#111'
+      }
+    },
+    {
+      top: 340,
+      left: 'center',
+      range: ['2016-07-01', '2016-12-31'],
+      splitLine: {
+        show: true,
+        lineStyle: {
+          color: '#000',
+          width: 4,
+          type: 'solid'
+        }
       },
-      xAxis: {},
-      yAxis: {},
-      calendar: [
-        {
-          top: 100,
-          left: "center",
-          // range: ["2023-9-01", "2023-11-30"],
-          splitLine: {
-            show: true,
-            lineStyle: {
-              color: "#000",
-              width: 4,
-              type: "solid",
-            },
-          },
-          yearLabel: {
-            formatter: "{start}  1st",
-            color: "#fff",
-          },
-          itemStyle: {
-            color: "#323c48",
-            borderWidth: 1,
-            borderColor: "#111",
-          },
-        },
-        {
-          top: 340,
-          left: "center",
-          // range: ["2016-07-01", "2016-12-31"],
-          splitLine: {
-            show: true,
-            lineStyle: {
-              color: "#000",
-              width: 4,
-              type: "solid",
-            },
-          },
-          yearLabel: {
-            formatter: "{start}  2nd",
-            color: "#fff",
-          },
-          itemStyle: {
-            color: "#323c48",
-            borderWidth: 1,
-            borderColor: "#111",
-          },
-        },
-      ],
-      series: generateMockSeries(generateData()), // 使用类型断言
+      yearLabel: {
+        formatter: '{start}  2nd',
+        color: '#fff'
+      },
+      itemStyle: {
+        color: '#323c48',
+        borderWidth: 1,
+        borderColor: '#111'
+      }
+    }
+  ],
+  series: [
+    {
+      name: 'Steps',
+      type: 'scatter',
+      coordinateSystem: 'calendar',
+      data: data,
+      symbolSize: function (val) {
+        return val[1] / 500;
+      },
+      itemStyle: {
+        color: '#ddb926'
+      }
+    },
+    {
+      name: 'Steps',
+      type: 'scatter',
+      coordinateSystem: 'calendar',
+      calendarIndex: 1,
+      data: data,
+      symbolSize: function (val) {
+        return val[1] / 500;
+      },
+      itemStyle: {
+        color: '#ddb926'
+      }
+    },
+    {
+      name: 'Top 12',
+      type: 'effectScatter',
+      coordinateSystem: 'calendar',
+      calendarIndex: 1,
+      data: data
+        .sort(function (a, b) {
+          return b[1] - a[1];
+        })
+        .slice(0, 12),
+      symbolSize: function (val) {
+        return val[1] / 500;
+      },
+      showEffectOn: 'render',
+      rippleEffect: {
+        brushType: 'stroke'
+      },
+      itemStyle: {
+        color: '#f4e925',
+        shadowBlur: 10,
+        shadowColor: '#333'
+      },
+      zlevel: 1
+    },
+    {
+      name: 'Top 12',
+      type: 'effectScatter',
+      coordinateSystem: 'calendar',
+      data: data
+        .sort(function (a, b) {
+          return b[1] - a[1];
+        })
+        .slice(0, 12),
+      symbolSize: function (val) {
+        return val[1] / 500;
+      },
+      showEffectOn: 'render',
+      rippleEffect: {
+        brushType: 'stroke'
+      },
+      itemStyle: {
+        color: '#f4e925',
+        shadowBlur: 10,
+        shadowColor: '#333'
+      },
+      zlevel: 1
+    }
+  ]
     };
     heatMap.setOption(option);
   }
